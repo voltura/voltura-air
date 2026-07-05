e86abe904e67ce0ddfc34a282e33f5bb4323dd76
Joakim Skoglund <joakim@voltura.se>
2026-07-05 11:54:35 +0200

    Fix permission visual brush alias

--- apps/windows-host/MainWindow.PermissionChoiceVisuals.cs
+++ apps/windows-host/MainWindow.PermissionChoiceVisuals.cs
@@ -1,7 +1,7 @@
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 using Button = System.Windows.Controls.Button;
+using Brush = System.Windows.Media.Brush;
 
 namespace VolturaAir.Host;
 

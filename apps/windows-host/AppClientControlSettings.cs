7513d4f42c5182705c1195a7807d1e5135207667
Joakim Skoglund <joakim@voltura.se>
2026-07-06 03:54:18 +0200

    Fix client control setting persistence

--- apps/windows-host/AppClientControlSettings.cs
+++ apps/windows-host/AppClientControlSettings.cs
@@ -2,20 +2,22 @@ namespace VolturaAir.Host;
 
 public static class AppClientControlSettings
 {
+    private const string SettingsKeyPath = @"Software\VolturaAir";
     private const string ValueName = "AllowPairedDeviceHostControl";
 
     public static event EventHandler? Changed;
 
     public static bool IsEnabled()
     {
-        using var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(AppRegistry.SettingsKeyPath, writable: false);
+        using var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(SettingsKeyPath, writable: false);
         return key?.GetValue(ValueName) is int value && value != 0;
     }
 
     public static void SetEnabled(bool enabled)
     {
         var current = IsEnabled();
-        using var key = AppRegistry.OpenWritableSettingsKey();
+        using var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(SettingsKeyPath, writable: true) ??
+            Microsoft.Win32.Registry.CurrentUser.CreateSubKey(SettingsKeyPath, writable: true);
         key.SetValue(ValueName, enabled ? 1 : 0, Microsoft.Win32.RegistryValueKind.DWord);
 
         if (current != enabled)
